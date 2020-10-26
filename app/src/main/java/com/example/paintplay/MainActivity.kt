46b9a4d3c4f567a2a49ce87792221d570cbd339f
Xan-Kerem <XanKerem@protonmail.com>
2020-10-26 21:48:21 +0330

    brashSizeChooser added!

--- app/src/main/java/com/example/paintplay/MainActivity.kt
+++ app/src/main/java/com/example/paintplay/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.paintplay
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.paintplay.databinding.ActivityMainBinding
+import com.example.paintplay.databinding.DialogBrushSizeBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +15,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.drawingView.setSizeForBrush(20f)
+        binding.drawingView.setSizeForBrush(5f)
 
+        binding.brushIb.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+
+    }
+
+    private fun showBrushSizeChooserDialog() {
+        val brushDialog = Dialog(this)
+
+        val dBinding = DialogBrushSizeBinding.inflate(layoutInflater)
+        brushDialog.setContentView(dBinding.root)
+
+        brushDialog.setTitle("Brush size")
+        dBinding.smallBrushIb.setOnClickListener {
+            binding.drawingView.setSizeForBrush(10f)
+            brushDialog.dismiss()
+        }
+        dBinding.mediumBrushIb.setOnClickListener {
+            binding.drawingView.setSizeForBrush(20f)
+            brushDialog.dismiss()
+        }
+        dBinding.largeBrushIb.setOnClickListener {
+            binding.drawingView.setSizeForBrush(30f)
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
     }
 }
