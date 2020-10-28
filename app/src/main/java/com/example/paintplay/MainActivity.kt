5ec9399510994f6ecedc7a8da480ff790c2bc45b
Xan-Kerem <XanKerem@protonmail.com>
2020-10-28 22:18:46 +0330

    share to other app added!

--- app/src/main/java/com/example/paintplay/MainActivity.kt
+++ app/src/main/java/com/example/paintplay/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
@@ -91,6 +92,18 @@ class MainActivity : AppCompatActivity() {
                             cancelProgressDialog()
 
                             Toast.makeText(this@MainActivity, result, Toast.LENGTH_SHORT).show()
+
+                            MediaScannerConnection.scanFile(this@MainActivity, arrayOf(result),null){
+                                path, uri ->
+
+                                val shareIntent = Intent()
+                                shareIntent.action = Intent.ACTION_SEND
+                                shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+                                shareIntent.type = "image/png"
+
+                                startActivity(Intent.createChooser(shareIntent, "Share"))
+                            }
+
                         }
                     } else {
                         withContext(Dispatchers.Main) {
