e950d42560f710a5da33972281b3cf2c6cf605ed
Xan-Kerem <XanKerem@protonmail.com>
2020-10-28 16:58:52 +0330

    image as a background added!

--- app/src/main/java/com/example/paintplay/MainActivity.kt
+++ app/src/main/java/com/example/paintplay/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.paintplay
 
+import android.Manifest
+import android.app.Activity
 import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import com.example.paintplay.databinding.ActivityMainBinding
@@ -36,6 +43,38 @@ class MainActivity : AppCompatActivity() {
             showBrushSizeChooserDialog()
         }
 
+        binding.galleryIb.setOnClickListener {
+            if (isReadStorageAllowed()) {
+                // run gallery code
+                val pickPhotoIntent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+                startActivityForResult(pickPhotoIntent, GALLERY)
+
+            } else {
+                requestStoragePermission()
+            }
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GALLERY) {
+                try {
+                    if (data?.data != null) {
+                        binding.backIv.setImageURI(data.data)
+                    }
+                } catch (e: Exception) {
+                    Toast.makeText(
+                        this,
+                        "Something went wrong with loading image",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
     }
 
     private fun showBrushSizeChooserDialog() {
@@ -87,5 +126,53 @@ class MainActivity : AppCompatActivity() {
 
             currentPaintImageButton = view
         }
+    }
+
+    private fun requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ).toString()
+            )
+        ) {
+            Toast.makeText(this, "need permission to add back image", Toast.LENGTH_SHORT).show()
+        }
+        ActivityCompat.requestPermissions(
+            this, arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ), STORAGE_PERMISSION_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                Toast.makeText(this, "permission granted!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "oops u just denied the permission!", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    private fun isReadStorageAllowed(): Boolean {
+        val result =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private const val STORAGE_PERMISSION_CODE = 1
+        private const val GALLERY = 2
     }
 }
